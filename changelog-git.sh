eb8776df527eed48a4c0a25e7c6b3bed440951e5
sinfallas <sinfallas@gmail.com>
2014-11-30 12:18:01 -0430

    * se eliminan los script que ya fueron incorporados a mygit.sh

--- changelog-git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git pull
-git log --decorate > changelog
-git add .
-git commit -a -m "* changelog"
-git push --all
-exit 0
--- subir-git.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git pull
-git add .
-git commit -a
-git push --all
-exit 0
